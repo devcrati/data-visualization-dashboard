f17a9e3b0c12ef965b30ce4b5656cc2c47a6b630
devcrati <59236904+devcrati@users.noreply.github.com>
2025-03-11 19:44:26 -0500

    feat: update welcome message

--- client/src/App.vue
+++ client/src/App.vue
@@ -9,7 +9,7 @@ import DashboardView from './views/DashboardView.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="Edge Network Revenue Dashboard" />
     </div>
   </header>
 
